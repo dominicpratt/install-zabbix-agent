cffbfd111c0aa78dd8bb32095c909aa94ed30146
Dominic Pratt <hallo@dominicpratt.de>
2019-12-24 14:28:40 +0100

    First useful commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# install-zabbix-agent.sh
+
+This is a tiny little bash script to install zabbix-agent and create the configurations needed.
+
+It creates:
+
+* PSK-Identity and PSK-Key
+* /etc/zabbix-agent/zabbix_agentd.conf
+
+## Dependencies
+
+As I'm using this or a similar script on all my servers I've just tested this against software that I use.
+
+* Debian Buster (10)
+
+## Usage
+
+Just run the script directly (please, for gods sake - check the code prior to execution):
+
+```
+bash <(curl -s https://github.com/dominicpratt/install-zabbix-agent/blob/master/install-zabbix-agent.sh)
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- install-zabbix-agent.sh
+++ install-zabbix-agent.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Set some needed variables
 PKIIDENTITY=$RANDOM
 PSKKEY=`openssl rand -hex 32`
 DEBIANVERSION=`cat /etc/debian_version`
 ZABBIXHOST=""
 
+# Get hostname of Zabbix-Server
 if [ -z "$ZABBIXHOST" ]; then
 	echo -n "==> Please input the hostname of your Zabbix Monitoring Server... [zabbix.example.org]: "
 	read -r ZABBIXHOST
 fi
 
+# Get Debian version
 if [[ $DEBIANVERSION == *"10"* ]]; then
         wget -q https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1%2Bbuster_all.deb -O /tmp/zabbix.deb
 elif [[ $DEBIANVERSION == *"9"* ]]; then
@@ -18,26 +21,32 @@ elif [[ $DEBIANVERSION == *"8"* ]]; then
         wget -q https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1%2Bjessie_all.deb -O /tmp/zabbix.deb
 fi
 
+# Install Zabbix-Repository, then update sources and install Zabbix-Agent and OpenSSL
 dpkg -i /tmp/zabbix.deb >/dev/null 2>&1
 apt-get -qq update >/dev/null 2>&1
 apt-get -qq install zabbix-agent openssl -y >/dev/null 2>&1
+
+# Deploy PSKKEY
 echo $PSKKEY > /etc/zabbix/zabbix_agentd.psk
 
+# Generate Zabbix-Agent-Configuration
 cat <<EOT > /etc/zabbix/zabbix_agentd.conf
-    PidFile=/var/run/zabbix/zabbix_agentd.pid
-    LogFile=/var/log/zabbix/zabbix_agentd.log
-    LogFileSize=0
-    Server=$ZABBIXHOST
-    ServerActive=$ZABBIXHOST
-    Hostname=`hostname -f`
-    Include=/etc/zabbix/zabbix_agentd.d/*.conf
-    TLSConnect=psk
-    TLSAccept=psk
-    TLSPSKIdentity=PKI$PKIIDENTITY
-    TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
+PidFile=/var/run/zabbix/zabbix_agentd.pid
+LogFile=/var/log/zabbix/zabbix_agentd.log
+LogFileSize=0
+Server=$ZABBIXHOST
+ServerActive=$ZABBIXHOST
+Hostname=`hostname -f`
+Include=/etc/zabbix/zabbix_agentd.d/*.conf
+TLSConnect=psk
+TLSAccept=psk
+TLSPSKIdentity=PKI$PKIIDENTITY
+TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
 EOT
 
+# Show configuration details
 echo "The PKI-Identity for this host is: PKI$PKIIDENTITY"
 echo "The PSK-Key for this host is: $PSKKEY"
 
+# Restart Zabbix-Agent
 service zabbix-agent restart >/dev/null 2>&1
